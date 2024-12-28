606e50e5118ba58e401e55b7ba3e35a96b0c5672
hhemu863 <hhemu863@gmail.com>
2024-12-28 18:44:18 +0530

    props Concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./About";
+import Contact from "./Contact";
+import Greeting from "./Greeting";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello from ReactJs</h1>
+    <Greeting name = {'Akash'}/>
+    <Greeting name = {'Ayush'}/>
+    {/*<Home name = {'Pooja'} age = {24}/>
+    <About />
+    <Contact />*/}
+    </>
   );
 }
 
---
+++ basics/src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return(
+        <>
+        <h1>Header Compnonet</h1>
+        <header>Some Header Content</header>
+        </>
+    );
+}
+
+export default Header;
